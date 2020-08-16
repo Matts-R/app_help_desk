8a4cabc349566aa52df96e0e5df32df877222591
Matheus H. Ribeiro <matheushrdasilva13@gmail.com>
2020-08-16 11:01:13 -0300

    Melhor explicação sobre a funcionalidade do script

--- validador_acesso.php
+++ validador_acesso.php
@@ -5,7 +5,10 @@
 session_start();
 
 if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] !== 'Autenticado') {
+  /*Verificando se o campo 'autenticado' da super global $_SESSION está vazio ou se seu valor é diferente de 'Autenticado', porque 
+  nesses casos o login será negado e o usuário será mandado de volta para a página de login
+  */
   header('Location: index.php?login=acesso_negado');
+  //Usando a função header para mandar o usuário de volta para a página de login, passando como parâmetro na URL o valor de login=acesso_negado
 }
-// Restringindo o acesso as demais páginas caso a sessão não tenha sido iniciada ou se seu valor não for igual a 'Autenticado'
 ?>
