8e5d53ff498e7c7fa48cfe8eacfc87e3be0a3659
Matheus H. Ribeiro <matheushrdasilva13@gmail.com>
2020-08-16 10:56:05 -0300

    Melhorando a explicação sobre o script.

--- valida_login.php
+++ valida_login.php
@@ -1,6 +1,7 @@
 <?php
 
 require "../../app_help_desk/valida_login.php";
-//Utilizando o token '../' para poder "subir um nível" na raiz do diretório para poder encontrar o arquivo com os scripts desejados. 
-//Tudo isso para fins demonstrativos, o intuito desse exemplo é demonstrar uma maneira de proteger a aplicação 
+/*Utilizando o token '../' para poder "subir um nível" na raiz do diretório para poder encontrar o arquivo com os scripts desejados. 
+Tudo isso para fins demonstrativos, o intuito desse exemplo é demonstrar uma maneira de proteger dados sensíveis aplicação.
+*/
 ?>
